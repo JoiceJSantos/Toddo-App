d9e7c9bce57aa1a048102956fc2f6f32d69c6356
Joice <joicitias@hotmail.com>
2020-11-03 09:49:12 -0300

    Atualização todo e todolist

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,8 +13,20 @@ export default class Todo extends Component{
         this.state = {description: '', list:[]}
 
         this.handleChange = this.handleChange.bind(this)
-
         this.handleAdd = this.handleAdd.bind(this)
+        
+        this.handleRemove = this.handleRemove.bind(this)
+        
+
+        this.refresh()
+    }
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description:'',list: resp.data}))
     }
     handleChange(e){
         this.setState({...this.state, description:e.target.value})
@@ -22,7 +34,7 @@ export default class Todo extends Component{
     handleAdd(){
        const description = this.state.description
        axios.post(URL, {description})
-        .then(resp =>console.log('funcionou!!'))
+        .then(resp =>this.refresh())
     }
     render(){
         return(
@@ -31,7 +43,8 @@ export default class Todo extends Component{
                 <TodoForm description={this.state.description}
                 handleAdd={this.handleAdd}
                 handleChange={this.handleChange}/>
-                <TodoList/>
+                <TodoList list={this.state.list}
+                    handelRemove={this.handelRemove}/>
             </div>
         )
     }
